42dcf7889842195fd9839e9e172beeea71ffbff2
MulL1on <liangweijian666@outlook.com>
2023-09-27 22:48:32 +0800

    fix: fix logstash license

--- app/service/rpc/user/initialize/logger.go
+++ app/service/rpc/user/initialize/logger.go
@@ -2,6 +2,7 @@ package initialize
 
 import (
 	g "go-ssip/app/service/rpc/user/global"
+	"go.elastic.co/ecszap"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -12,13 +13,13 @@ import (
 func InitLogger(serviceName string) {
 	dynamicalLevel := zap.DebugLevel
 
-	encoder := getEncoder()
+	encoder := getECSEncoder()
 	cores := [...]zapcore.Core{
-		zapcore.NewCore(encoder, os.Stdout, dynamicalLevel),
-		zapcore.NewCore(
-			encoder,
+		ecszap.NewCore(encoder, os.Stdout, dynamicalLevel),
+		ecszap.NewCore(
+			getECSEncoder(),
 			zapcore.AddSync(&lumberjack.Logger{
-				Filename:   "tmp/" + serviceName + ".log",
+				Filename:   "tmp/" + serviceName + ".json",
 				MaxSize:    5,
 				MaxAge:     400,
 				MaxBackups: 1000,
@@ -27,7 +28,7 @@ func InitLogger(serviceName string) {
 			}),
 			dynamicalLevel),
 	}
-	g.Logger = zap.New(zapcore.NewTee(cores[:]...), zap.AddCaller())
+	g.Logger = zap.New(zapcore.NewTee(cores[:]...), zap.AddCaller()).With(zap.String("app", serviceName))
 
 	defer func(Logger *zap.Logger) {
 		_ = Logger.Sync()
@@ -53,4 +54,15 @@ func getEncoder() zapcore.Encoder {
 
 func customTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(t.Format("[2006-01-02 15:04:05.000]"))
+}
+
+func getECSEncoder() ecszap.EncoderConfig {
+	cfg := ecszap.NewDefaultEncoderConfig()
+	cfg.EncodeCaller = zapcore.FullCallerEncoder
+	cfg.EncodeLevel = zapcore.CapitalLevelEncoder
+	cfg.EnableStackTrace = true
+	cfg.EncodeDuration = zapcore.StringDurationEncoder
+	cfg.LineEnding = zapcore.DefaultLineEnding
+	cfg.EnableCaller = true
+	return cfg
 }
